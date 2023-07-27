90adddbad7406400ac568963809c6feedc03e2ad
unknown <farley2181@hotmail.com>
2023-07-27 06:59:29 -0300

    dei uma melhorada na aplicação ultilizando o conceito de classe e model.

--- buscador_de_cep.dart
+++ buscador_de_cep.dart
@@ -1,5 +1,25 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'dart:io';
+
+class ViaCepModel {
+  final String logradouro;
+  final String bairro;
+  final String localidade;
+  final String uf;
+
+  ViaCepModel({
+    required this.logradouro,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+  });
+
+  @override
+  String toString() {
+    return "Com base no cep informado o endereço encontrado foi: $logradouro, $bairro, $localidade - $uf)";
+  }
+}
 
 Future<void> main() async {
   print("Digite o cep para descobrir o endereço: ");
@@ -7,7 +27,17 @@ Future<void> main() async {
   final url = Uri.parse('https://viacep.com.br/ws/$cep/json/');
   final response = await http.get(url);
 
+  ViaCepModel? cepModel;
+
   if (response.statusCode == 200) {
-    print(response.body);
+    final map = json.decode(response.body);
+
+    cepModel = ViaCepModel(
+      logradouro: map['logradouro'],
+      bairro: map['bairro'],
+      localidade: map['localidade'],
+      uf: map['uf'],
+    );
   }
+  print("RESULTADO: $cepModel");
 }
